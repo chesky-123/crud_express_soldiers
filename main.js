8bf84380a5dd61a577f6ae2d3158aa926c3a56b4
chesky <sye0534184736@gmail.com>
2026-07-15 16:48:55 +0300

    connect rout to server

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
+import { router } from "./routes/soldiersRouter.js";
+import express from "express";
+
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/soldiers",router);
+
+
+app.listen(PORT,(e)=>{
+    if (e) return console.log(e);
+    
+    console.log("server running in port",PORT,"...");
+    
+});
+
 
 
--- routes/soldiersRouter.js
+++ routes/soldiersRouter.js
@@ -1,3 +1,7 @@
+import express from "express";
+
+export const router = express.Router();
+
 
 
 
